221a5ede8720a65aba013212dc8ab88f8aaac775
lioneljones <lioneljones5116@gmail.com>
2022-04-27 20:40:07 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+      /* don't expose password or any sensitive info, done only for demo */
+      host: "localhost",
+      user: "root",
+      password: "Mag17615@",
+      database: "hisdnutritionalservices",
+    },
+    listPerPage: 1000,
+  };
+  module.exports = config;
---
+++ index.js
@@ -0,0 +1,58 @@
+//https://blog.logrocket.com/build-rest-api-node-express-mysql/
+//npm i express
+/*
+npm i mysql2
+npm i cors  -save
+Adding a package.json file
+https://www.youtube.com/watch?v=_eRwjuIDJ2Y
+To add a package.json file to your node.js project
+Type:
+npm init
+It will ask you a series of questions, just take some defaults
+Then you have to do an npm install on all of the dependencies you installed before but using the -save
+keyword. I:e
+npm install mysql -save
+*/
+
+
+const express = require("express");
+
+//wire up cors
+var cors = require('cors')
+
+const app = express();
+app.use(cors())
+const port = 4051;
+
+//make reference to your router
+const router = require("./routes/route");
+
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+//this is your default route /
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+
+//with your router helper, create a /programming-languages  (route)
+// http://localhost:4051/hisdnutritionalservices
+app.use("/hisdnutritionalservices", router);
+
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({ message: err.message });
+  return;
+});
+
+//create the port listener
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
